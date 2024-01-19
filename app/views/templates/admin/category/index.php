d14872c6c59ae6db7e57a4a3ecb702e5fa3e4424
Alex Cherniakov <rustavellyne@gmail.com>
2024-01-19 10:42:39 +0200

    add Root node

--- app/views/templates/admin/category/index.php
+++ app/views/templates/admin/category/index.php
@@ -22,50 +22,65 @@
             <div class="col-12">
                 <?php if(!empty($tree)): ?>
                     <ul class="list-group">
-                <?php foreach ($tree as $node): ?>
-                    <li class="list-group-item d-flex justify-content-between">
-                        <span><?= $node['title']?></span>
-                        <div class="btn-group">
-                            <a href="/admin/category/add?id=<?= $node['id'] ?>" class="btn btn-success btn-block mt-2"><i class="fa fa-plus"></i>
-                            </a>
-                            <a href="/admin/category/remove?id=<?= $node['id'] ?>" class="btn btn-danger btn-block"><i class="fa fa-trash"></i>
-                            </a>
-                        </div>
-                    </li>
-                    <?php if (isset($node['children']) && is_array($node['children'])): ?>
-                        <ul class="list-group ml-5">
-                            <?php foreach ($node['children'] as $node1): ?>
-                                <li class="list-group-item d-flex justify-content-between">
-                                    <span><?= $node1['title']?></span>
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            <span> Root </span>
+                            <div class="btn-group">
+                                <a href="/admin/category/add?id=0" class="btn btn-success btn-block m-0"><i class="fa fa-plus"></i>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <ul class="list-group">
+                            <?php foreach ($tree as $node): ?>
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                    <span><?= $node['title']?></span>
                                     <div class="btn-group">
-                                        <a href="/admin/category/add?id=<?= $node1['id'] ?>" class="btn btn-success btn-block mt-2"><i class="fa fa-plus"></i>
+                                        <a href="/admin/category/add?id=<?= $node['id'] ?>" class="btn btn-success btn-block m-0"><i class="fa fa-plus"></i>
                                         </a>
-                                        <a href="/admin/category/remove?id=<?= $node1['id'] ?>" class="btn btn-danger btn-block"><i class="fa fa-trash"></i>
+                                        <a href="/admin/category/remove?id=<?= $node['id'] ?>" class="btn btn-danger btn-block m-0"><i class="fa fa-trash"></i>
                                         </a>
                                     </div>
                                 </li>
-                                <?php if (isset($node1['children']) && is_array($node1['children'])): ?>
-                                    <ul class="list-group ml-5">
-                                        <?php foreach ($node1['children'] as $node2): ?>
-                                            <li class="list-group-item d-flex justify-content-between">
-                                                <span><?= $node2['title']?></span>
+                                <?php if (isset($node['children']) && is_array($node['children'])): ?>
+                                    <li class="list-group-item">
+                                        <ul class="list-group ml-5">
+                                        <?php foreach ($node['children'] as $node1): ?>
+                                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                <span><?= $node1['title']?></span>
                                                 <div class="btn-group">
-                                                    <a href="/admin/category/add?id=<?= $node2['id'] ?>" class="btn btn-success btn-block mt-2"><i class="fa fa-plus"></i>
+                                                    <a href="/admin/category/add?id=<?= $node1['id'] ?>" class="btn btn-success btn-block m-0"><i class="fa fa-plus"></i>
                                                     </a>
-                                                    <a href="/admin/category/remove?id=<?= $node2['id'] ?>" class="btn btn-danger btn-block"><i class="fa fa-trash"></i>
+                                                    <a href="/admin/category/remove?id=<?= $node1['id'] ?>" class="btn btn-danger btn-block m-0"><i class="fa fa-trash"></i>
                                                     </a>
                                                 </div>
                                             </li>
-                                            <?php if (isset($node2['children']) && is_array($node2['children'])): ?>
+                                            <?php if (isset($node1['children']) && is_array($node1['children'])): ?>
+                                            <li class="list-group-item">
+                                                <ul class="list-group ml-5">
+                                                    <?php foreach ($node1['children'] as $node2): ?>
+                                                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                            <span><?= $node2['title']?></span>
+                                                            <div class="btn-group">
+                                                                <a href="/admin/category/add?id=<?= $node2['id'] ?>" class="btn btn-success btn-block m-0"><i class="fa fa-plus"></i>
+                                                                </a>
+                                                                <a href="/admin/category/remove?id=<?= $node2['id'] ?>" class="btn btn-danger btn-block m-0"><i class="fa fa-trash"></i>
+                                                                </a>
+                                                            </div>
+                                                        </li>
+                                                        <?php if (isset($node2['children']) && is_array($node2['children'])): ?>
 
+                                                        <?php endif ?>
+                                                    <?php endforeach; ?>
+                                                </ul>
+                                            </li>
                                             <?php endif ?>
                                         <?php endforeach; ?>
-                                    </ul>
+                                        </ul>
+                                    </li>
                                 <?php endif ?>
                             <?php endforeach; ?>
-                        </ul>
-                    <?php endif ?>
-                <?php endforeach; ?>
+                            </ul>
+                        </li>
                     </ul>
                 <?php endif; ?>
             </div>
